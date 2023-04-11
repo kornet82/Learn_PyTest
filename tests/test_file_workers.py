f7281e1c98ae201db1b66c7a9f399630c28f6e6c
kornet82 <azarovp@gmail.com>
2023-04-12 00:58:37 +0200

    Lesson 2. Add modern tests --> clear .txt before test - write - pass

--- tests/test_file_workers.py
+++ tests/test_file_workers.py
@@ -2,7 +2,7 @@ from my_funcs.file_workers import read_from_file
 
 
 def test_read_from_file():
-    with open("tests/test_file.txt", "a") as f_o:
+    with open("tests/test_file.txt", "w") as f_o:
         pass
     test_data = ["one\n", "two\n", "three\n"]
     with open("tests/test_file.txt", "a") as f_o:
