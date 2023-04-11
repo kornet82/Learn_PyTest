02a0a07b44c5292509a8f2cff6046fd73996297b
kornet82 <azarovp@gmail.com>
2023-04-12 01:21:55 +0200

    Lesson 2. refactor

--- tests/test_file_workers/test_file_workers.py
+++ tests/test_file_workers/test_file_workers.py
@@ -1,10 +1,13 @@
 from my_funcs.file_workers import read_from_file
+import pytest
 
+def create_test_data(test_data):
+    with open("tests/test_file.txt", "a") as f_o:
+        f_o.writelines(test_data)
 
 def test_read_from_file():
     test_data = ["one\n", "two\n", "three\n"]
-    with open("tests/test_file.txt", "a") as f_o:
-        f_o.writelines(test_data)
+    create_test_data(test_data)
     assert test_data == read_from_file("tests/test_file.txt")
 
 
